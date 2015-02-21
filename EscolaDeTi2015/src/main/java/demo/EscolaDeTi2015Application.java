7ccb5ed8f5f781839495265eadc07f62d973e7a3
andrey.nakano <andreyhideki@gmail.com>
2015-02-21 14:56:18 -0200

    gravando projeto spring start

---
+++ EscolaDeTi2015/src/main/java/demo/EscolaDeTi2015Application.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaDeTi2015Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaDeTi2015Application.class, args);
+    }
+}
